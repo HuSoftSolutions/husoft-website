94372df3d7777343e475c53bf26d71b682b20182
codyhusek <cody.husek@husoftsolutions.com>
2023-08-13 00:40:47 -0400

    fix: meet our team

--- app/components/TeamSection.js
+++ app/components/TeamSection.js
@@ -8,33 +8,35 @@ const TeamSection = () => {
       </h2>
       {/* Render team members here */}
 
-      <div className="flex flex-col md:flex-row justify-around ">
+      <div className="flex flex-col lg:flex-row justify-around ">
         {/* Render team member cards */}
-
-        <div className=" shadow-lg transition-transform transform hover:scale-105 bg-slate-500 bg-opacity-40 p-4 rounded-lg w-full md:w-1/4">
-          <div className="mb-4 relative w-48 h-48 mx-auto">
-            <Image
-              src="/images/team/cody_headshot.jpeg"
-              layout="fill"
-              objectFit="cover"
-              className="rounded-full"
-            />
+        <div className="p-2 flex w-full lg:w-1/4">
+          <div className="shadow-lg transition-transform transform hover:scale-105 bg-slate-500 bg-opacity-40 p-4 rounded-lg w-full">
+            <div className="mb-4 relative w-40 h-40 mx-auto">
+              <Image
+                src="/images/team/cody_headshot.jpeg"
+                layout="fill"
+                objectFit="cover"
+                className="rounded-full"
+              />
+            </div>
+            <h3 className="text-xl font-semibold mb-2">Cody Husek</h3>
+            <p className="text-gray-800">Co-Founder, Lead Developer</p>
           </div>
-          <h3 className="text-xl font-semibold mb-2">Cody Husek</h3>
-          <p className="text-gray-800">Co-Founder, Lead Developer</p>
         </div>
-
-        <div className=" shadow-lg transition-transform transform hover:scale-105 bg-slate-500 bg-opacity-40 p-4 rounded-lg w-full md:w-1/4 mb-3 md:mb-0">
-          <div className="mb-4 relative w-48 h-48 mx-auto">
-            <Image
-              src="/images/team/tom_headshot.jpeg"
-              layout="fill"
-              objectFit="cover"
-              className="rounded-full"
-            />
+        <div className="p-2 flex w-full lg:w-1/4">
+          <div className=" shadow-lg transition-transform transform hover:scale-105 bg-slate-500 bg-opacity-40 p-4 rounded-lg w-full">
+            <div className="mb-4 relative w-40 h-40 mx-auto">
+              <Image
+                src="/images/team/tom_headshot.jpeg"
+                layout="fill"
+                objectFit="cover"
+                className="rounded-full"
+              />
+            </div>
+            <h3 className="text-xl font-semibold mb-2">Tom Mittler</h3>
+            <p className="text-gray-800">Co-Founder, Director of Operations</p>
           </div>
-          <h3 className="text-xl font-semibold mb-2">Tom Mittler</h3>
-          <p className="text-gray-800">Co-Founder, Director of Operations</p>
         </div>
       </div>
     </section>
